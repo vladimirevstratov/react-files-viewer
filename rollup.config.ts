625037ba2db9c093be6e079897703f00da71a321
Apollon Linux <apollonseven@gmail.com>
2021-03-30 01:41:38 +0300

    Fix rollup config

--- rollup.config.ts
+++ rollup.config.ts
@@ -4,7 +4,6 @@ import sourceMaps from 'rollup-plugin-sourcemaps'
 import typescript from 'rollup-plugin-typescript2'
 import json from 'rollup-plugin-json'
 import external from 'rollup-plugin-peer-deps-external'
-import serve from 'rollup-plugin-dev-server'
 
 import postcss from 'rollup-plugin-postcss'
 import url from 'rollup-plugin-url'
@@ -34,8 +33,7 @@ export default {
       sourcemap: true
     }
   ],
-  // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-  external: ['react'],
+  external: [],
   watch: {
     include: 'src/**'
   },
@@ -60,20 +58,8 @@ export default {
     // Allow node_modules resolution, so you can use 'external' to control
     // which external modules to include in the bundle
     // https://github.com/rollup/rollup-plugin-node-resolve#usage
-    resolve({
-      browser: true
-    }),
-
+    resolve(),
     // Resolve source maps to the original source
-    sourceMaps(),
-    serve({
-      // Set to true to allow cors request
-      allowCrossOrigin: true,
-
-      // set headers
-      headers: {
-        'Access-Control-Allow-Origin': '*'
-      }
-    })
+    sourceMaps()
   ]
 }
